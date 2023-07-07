c4c6d64577b6ef946cc2ff663d9f4ff04ad31662
danila.petrov <danilapetrov7856@gmail.com>
2023-07-07 12:14:18 +0300

    Уже не помню что менял)

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Config from 'react-native-config';
 
+console.log(Config);
 export const instance = axios.create({
   baseURL: `http://${Config.HOST}:${Config.PORT}`,
 });
